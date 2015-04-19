1e58669e0d6643814c2bfbbaee115e3554d03edc
T. <09000255@studentmail.ul.ie>
2015-04-19 22:02:04 +0100

    Update Logic.h

--- Logic.h
+++ Logic.h
@@ -6,24 +6,19 @@ using namespace std;
 class Logic {
 
 private:
-vector<Vehicle*> cars;
-vector<Layout*> roads;
-bool stop;
+	vector<Vehicle*> cars;
+	vector<Layout*> roads;
+	bool stop;
 
 public:
-    Logic();
-    Logic(vector<Vehicle*> cars, vector<Layout*> roads );
-    ~Logic();
-    vector<Vehicle*> getCars();
-    void setCars(vector<Vehicle*> cars);
-    vector<Layout*> getRoads();
-    void setRoads(vector<Layout*> roads);
-    void scan();
+	Logic();
+	Logic(vector<Vehicle*> cars, vector<Layout*> roads);
+	~Logic();
+	vector<Vehicle*> getCars();
+	void setCars(vector<Vehicle*> cars);
+	vector<Layout*> getRoads();
+	void setRoads(vector<Layout*> roads);
+	void scan();
 	bool simulationOver();
 
-   // int recordAverageSpeed(Vehicle vehicle);
 };
-
-
-
-
